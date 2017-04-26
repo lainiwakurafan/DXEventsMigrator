3224dee346cb321a6f7ce966112a77d594e820ec
lainiwakurafan <lainiwakurafan@gmail.com>
2017-04-27 00:22:53 +0300

    Итерация по направлениям и годам обучения

--- DXEventsMigrator/MainWindow.xaml.cs
+++ DXEventsMigrator/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using DevExpress.Data.Filtering;
 using DevExpress.Xpo;
@@ -8,7 +11,10 @@ using SpbuEducation.TimeTable;
 using SpbuEducation.TimeTable.Appointments;
 using SpbuEducation.TimeTable.Appointments.Repositories;
 using SpbuEducation.TimeTable.Appointments.StorageInitializers;
+using SpbuEducation.TimeTable.BusinessObjects.Contingent;
+using SpbuEducation.TimeTable.BusinessObjects.Education;
 using SpbuEducation.TimeTable.BusinessObjects.Events;
+using SpbuEducation.TimeTable.DataProcessing;
 
 namespace DXEventsMigrator
 {
@@ -22,11 +28,14 @@ namespace DXEventsMigrator
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
             var watch = Stopwatch.StartNew();
 
-            DoSomeStuff();
+            await Task.Run(() =>
+            {
+                DoSomeStuffSeparated();
+            });
 
             watch.Stop();
 
@@ -36,17 +45,94 @@ namespace DXEventsMigrator
         private Session _session;
 
         private const string ConnectionStringName = "testDB";
+        private IEnumerable<Division> GetDivisions(Session session)
+        {
+            return new XPCollection<Division>(session);
+        }
+
+        private IEnumerable<StudyYear> GetStudyYears(Session session)
+        {
+            return new XPCollection<StudyYear>(session);
+        }
+
+        private XPCursor GetPatternStudyEvents(XPCollection<ContingentUnit> contingentUnits)
+        {
+            var cursor = new XPCursor(_session, typeof (Event), new GroupOperator(CriteriaHelper.PatternEvent,
+                new InOperator("ContingentUnit", contingentUnits)));
+            cursor.PageSize = 10;
+            return cursor;
+        } 
+
+        private void DoSomeStuffSeparated()
+        {
+            using (
+                _session =
+                    new TimeTableSession
+                    {
+                        ConnectionString = Configuration.GetConnectionStringByName(ConnectionStringName)
+                    })
+            {
+                var divisions = GetDivisions(_session);
+                foreach (var division in divisions)
+                {
+                    foreach (var studyYear in GetStudyYears(_session))
+                    {
+                        var contingentUnitsCriteria = CriteriaOperator.And(
+                            new BinaryOperator("Division", division),
+                            new BinaryOperator("CurrentStudyYear", studyYear));
+                        var contingentUnits = new XPCollection<ContingentUnit>(_session, contingentUnitsCriteria);
+                        var patternStudyEvents = GetPatternStudyEvents(contingentUnits);
+                        foreach (Event patternEvent in patternStudyEvents)
+                        {
+                            GC.Collect();
+                            var patternExceptionEvents = FindExceptions(patternEvent);
+
+                            ProcessWholePattern(patternEvent, patternExceptionEvents);
+                        }
+                    }
+                }
+            }
+        }
 
         private void DoSomeStuff()
         {
-            var cursor = new XPCursor(_session, typeof(Event), CriteriaHelper.PatternEvent);
             using (_session = new TimeTableSession { ConnectionString = Configuration.GetConnectionStringByName(ConnectionStringName) })
             {
+                var cursor = new XPCursor(_session, typeof(Event), CriteriaHelper.PatternEvent);
                 foreach (Event patternEvnt in cursor)
                 {
                     var patternExceptionEvents = FindExceptions(patternEvnt);
 
                     ProcessWholePattern(patternEvnt, patternExceptionEvents);
+                    
+                }
+                _session.Delete(cursor);
+                _session.Save(cursor);
+            }
+        }
+
+        private void DoSomeStuffWithXPCollection()
+        {
+            GC.Collect();
+            using (
+                _session =
+                    new TimeTableSession
+                    {
+                        ConnectionString = Configuration.GetConnectionStringByName(ConnectionStringName)
+                    })
+            {
+                using (
+                    XPCollection collection = new XPCollection(_session, typeof (Event),
+                        CriteriaHelper.PatternEvent))
+                {
+                    collection.TopReturnedObjects = 10;
+                    // Loop through all the objects. 
+                    foreach (Event patternEvnt in collection)
+                    {
+                        var patternExceptionEvents = FindExceptions(patternEvnt);
+
+                        ProcessWholePattern(patternEvnt, patternExceptionEvents);
+                    }
                 }
             }
         }
@@ -70,11 +156,11 @@ namespace DXEventsMigrator
 
                 var eventCreator = new EventCreator(appointmentsRepository.SchedulerStorage);
 
-                Boom(appts, patternEvnt, eventCreator);
+                Boom(appts, patternEvnt, eventCreator, patternExceptionEvents);
             }
         }
 
-        private void Boom(IEnumerable<TimeEventAppointment> appts, Event e, EventCreator eventCreator)
+        private void Boom(IEnumerable<TimeEventAppointment> appts, Event e, EventCreator eventCreator, IEnumerable<Event> patternExceptionEvents)
         {
             _session.BeginTransaction();
 
@@ -103,8 +189,12 @@ namespace DXEventsMigrator
                     }
                 }
             }
-
+            _session.Delete(patternExceptionEvents);
+            _session.Save(patternExceptionEvents);
+            e.Delete();
             _session.CommitTransaction();
         }
+
+        private int counter = 0;
     }
 }
